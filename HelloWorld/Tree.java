0a959973f69aa4cd7e19b03de549098289d30dec
Chandru <106470788+ChanBotCoder@users.noreply.github.com>
2023-09-13 21:08:50 +0530

    Create Tree.java

---
+++ HelloWorld/Tree.java
@@ -0,0 +1,19 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] agrs){
+    // I am a single line comment
+    
+ 
+    /* It would appear that I, humble though
+       I may be, have been chosen as the 
+       multi-line comment for this example */
+    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("Ready to get my hands dirty!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
