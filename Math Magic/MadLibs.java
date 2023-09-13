600fba2fc732000260987ea03fe1fa4e5c9939ae
Chandru <106470788+ChanBotCoder@users.noreply.github.com>
2023-09-13 21:12:48 +0530

    Create MadLibs.java

---
+++ Math Magic/MadLibs.java
@@ -0,0 +1,28 @@
+public class MadLibs {
+  /*
+  This program generates a mad libbed story.
+Author: Laura
+Date: 2/19/2049
+  */
+  	public static void main(String[] args){
+      
+      String name1 = "Chandru";
+      String adjectives1 = "Well";
+      String adjectives1 = "Good";
+      String adjectives1 = "Great";
+      String verb1 = "Walking";
+      String noun1 = "man";
+      String noun2 = "man";
+      String noun3 = "man";
+      String noun4 = "man";
+      String noun5 = "man";
+      String noun6 = "man";
+      int number = 960076776;
+      place1 = "Ooty";
+      
+
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+    }       
+}
